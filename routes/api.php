a2527862a96f34e141a165f5e63528f218376ae3
Gevorg Martirosyan <gevorg@melistics.co>
2021-12-06 15:53:54 +0400

    BD-1 fixing

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/projects', ['App\Http\Controllers\JiraController', 'createProject']);
 
+
+Route::post('/projects', ['App\Http\Controllers\JiraController', 'createProject']);
 Route::post('/issues', ['App\Http\Controllers\JiraController', 'createIssue']);
